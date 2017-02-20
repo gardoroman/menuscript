52ca3170974a5a7b5a26e6440454f2070175f25e
gardoroman <gardoroman@gmail.com>
2017-02-20 11:06:10 -0600

    Add basic models

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,10 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from config import basedir
+
 
 app = Flask(__name__)
+app.config.from_object('config')
+db = SQLAlchemy(app)
 
-from app import views
+from app import views, models
---
+++ app/models.py
@@ -0,0 +1,27 @@
+from app import db
+
+class Store(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    store_name = db.Column(db.String(64), index=True, unique=True)
+    address = db.Column(db.String(200), index=True, unique=True)
+    phone = db.Column(db.String(15))
+    cuisine_type = db.Column(db.String(50))
+    opening_time = db.Column(db.String(15))
+    closing_time = db.Column(db.String(15))
+    menus = db.relationship('Menu', backref='store', lazy='dynamic')
+
+    def __repr__(self):
+        return '<Store %r>' % (self.store_name)
+
+class Menu(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    category = db.Column(db.String(80))
+    store_id = db.Column(db.Integer, db.ForeignKey('store.id'))
+    items = db.relationship('Item', backref='menu', lazy='dynamic')
+
+class Item(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    item_name = db.Column(db.String(64), index=True, unique=True)
+    description = db.Column(db.String(80))
+    section = db.Column(db.String(30))
+    menu_id = db.Column(db.Integer, db.ForeignKey('menu.id'))
