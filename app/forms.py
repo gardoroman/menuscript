d30cb6936f6c028cad876ffdaf3a24b58794dc90
gardoroman <gardoroman@gmail.com>
2017-02-27 13:18:26 -0600

    Website is more RESTful

--- app/forms.py
+++ app/forms.py
@@ -1,8 +1,8 @@
-from flask_wtf import Form
+from flask_wtf import FlaskForm
 from wtforms import StringField, TextAreaField, RadioField
 from wtforms.validators import DataRequired, Length
 
-class StoreForm(Form):
+class StoreForm(FlaskForm):
     store_name = StringField('store_name', validators=[DataRequired()])
     # address = TextAreaField('address', [validators.required(), validators.length(max=200)])
     address = TextAreaField('address', validators=[DataRequired()])
@@ -12,13 +12,13 @@ class StoreForm(Form):
     closing_time = StringField('closing_time', validators=[Length(min=0,max=15)])
 
 
-class MenuForm(Form):
+class MenuForm(FlaskForm):
     category = StringField('cuisine_type', validators=[Length(min=0,max=80)])
     # gave the RadioField the old college try :(
     # category = RadioField('Label', choices=[('1','Breakfast'),
     #                      ('2','Lunch'),('3','Dinner')])
 
-class ItemForm(Form):
+class ItemForm(FlaskForm):
     item_name = StringField('item_name', validators=[DataRequired()])
     description = StringField('description', validators=[DataRequired()])
     section =  StringField('section', validators=[DataRequired()])
